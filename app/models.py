69da0ed271816af063e21610d82505db3cd4ed1e
simonkairu <simon.mureithi@student.moringaschool.com>
2021-10-31 22:36:37 +0300

    first Commit

---
+++ app/models.py
@@ -0,0 +1,21 @@
+class Sources:
+
+    def __init__(self, id, name, description, url, category, country, language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
+
+class Articles:
+
+    def __init__(self, id, author,  description, url, image, date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date
---
+++ start.sh
@@ -0,0 +1,5 @@
+export NEWS_API_KEY='c2ad6778cf264bf89b698f3f07d1477f'
+# c2ad6778cf264bf89b698f3f07d1477f
+# export SECRET_KEY='1234'
+
+python3.8 manage.py server
